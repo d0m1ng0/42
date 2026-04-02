e23fe47d3a254868c92090f557e01206d778bb54
d0m1ng0 <domingo.verdini@gmail.com>
2026-04-02 14:21:13 +0200

    ex00: preparation

---
+++ 42-agentic-cursus/rank02/Python_Modules/module00-Growing_Code/ex00/hello_garden.py
@@ -0,0 +1,24 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    hello_garden.py                                    :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: dverdini <dverdini@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2026/04/02 14:17:07 by dverdini          #+#    #+#              #
+#    Updated: 2026/04/02 14:19:41 by dverdini         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+def	format_greeting(name: str = "Tutor") -> str:
+	"""
+	create an hello formatted for the garden.
+
+	Args:
+		name (str): The name of the person to say hello. Defaul: "Tutor".
+
+	Returns:
+		str: The message hello complete.
+	"""
+	# The logic is here
+	pass
